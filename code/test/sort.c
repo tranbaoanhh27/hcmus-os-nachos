d1c3aab72da4199f82081bb0597389259fd7f258
tranbaoanhh27 <tranbaoanhh27@gmail.com>
2022-10-15 13:29:39 +0000

    Changed program name bubblesort to sort.

--- code/test/sort.c
+++ code/test/sort.c
@@ -1,69 +1,83 @@
-/* sort.c 
- *    Test program to sort a large number of integers.
- *
- *    Intention is to stress virtual memory system.
- *
- *    Ideally, we could read the unsorted array off of the file system,
- *	and store the result back to the file system!
- */
-
-
-/*
-#define UNIX
-#define UNIX_DEBUG
-*/
-
-#ifdef UNIX
-#include <stdio.h>
-#define Exit exit
-#else
 #include "syscall.h"
-#endif /* UNIX */
 
-#define SIZE (1024)
+int GoodOrder(int curr, int next, int asc) {
+    if (asc == 1)
+        return (curr <= next) ? 1 : 0;
+    else
+        return (curr >= next) ? 1 : 0;
+}
 
-int A[SIZE];	/* size of physical memory; with code, we'll run out of space!*/
-
-int
-main()
-{
-    int i, j, tmp;
-
-    /* first initialize the array, in reverse sorted order */
-    for (i = 0; i < SIZE; i++) {
-        A[i] = (SIZE-1) - i;
-    }
-
-    /* then sort! */
-    for (i = 0; i < SIZE; i++) {
-        for (j = 0; j < (SIZE-1); j++) {
-	   if (A[j] > A[j + 1]) {	/* out of order -> need to swap ! */
-	      tmp = A[j];
-	      A[j] = A[j + 1];
-	      A[j + 1] = tmp;
-    	   }
+void BubbleSort(int* a, int n, int asc) {
+    int left, right;
+    left = 0;
+    right = n - 1;
+    while (left <= right) {
+        int isSwapped, lastSwapPos, j;
+        isSwapped = 0;
+        lastSwapPos = left + 1;
+        for (j = left; j < right; ++j) {
+            if (!GoodOrder(a[j], a[j+1], asc)) {
+                int temp;
+                temp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = temp;
+                isSwapped = 1;
+                lastSwapPos = j + 1;
+            }
         }
+        if (isSwapped == 0)
+            break;
+        right = lastSwapPos;
+    }
+}
+
+int main() {
+    
+    int n;
+    int a[100];
+    int isAscending, i;
+    
+    // Get size of array
+    do {
+        PrintString("Enter Size of array: ");
+        n = ReadNum();
+
+        if (n < 1 || n > 100)
+            PrintString("Invalid size! Please enter size as an integer in range [1, 100]\n");
+
+    } while (n < 1 || n > 100);
+
+    // Get array elements' values
+    PrintString("Enter elements' values:\n");
+    
+    for (i = 0; i < n; i++) {
+        PrintString("Enter value of array[");
+        PrintNum(i);
+        PrintString("]: ");
+        a[i] = ReadNum();
+    }
+    
+    // Get user choosen sort order
+    PrintString("Ascending or Descending (Enter 1 for ascending, 2 for descending): ");
+    do {
+        isAscending = ReadNum();
+        if (!(isAscending == 1 || isAscending == 2))
+            PrintString("Invalid choosen! Please enter 1 for ascending, 2 for descending)\n");
+    } while (isAscending != 1 && isAscending != 2);
+
+    PrintString("\nArray: ");
+    for (i = 0; i < n; i++) {
+        PrintNum(a[i]);
+        PrintChar(' ');
     }
 
-#ifdef UNIX_DEBUG
-    for (i=0; i<SIZE; i++) {
-        printf("%4d ", A[i]);
-	if (((i+1) % 15) == 0) {
-		printf("\n");
-        }
-        if (A[i] != i) {
-            fprintf(stderr, "Out of order A[%d] = %d\n", i, A[i]);
-            Exit(1);
-        }   
-    }
-    printf("\n");
-#endif /* UNIX_DEBUG */
+    BubbleSort(a, n, isAscending);
 
-    for (i=0; i<SIZE; i++) {
-        if (A[i] != i) {
-            Exit(1);
-        }   
+    PrintString("\nResult: ");
+    for (i = 0; i < n; i++) {
+        PrintNum(a[i]); PrintChar(' ');
     }
+    PrintChar('\n');
 
-    Exit(0);
+    Halt();
 }
