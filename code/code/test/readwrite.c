a92a8c2af3db4979f47392ab5d1acae3758a7b0e
leanhuynh <leanhuynh2002@gmail.com>
2022-11-19 21:26:38 +0700

    update file test: readwrite.c

--- code/code/test/readwrite.c
+++ code/code/test/readwrite.c
@@ -4,24 +4,28 @@ int
 main() {
     char buffer1[20];
     int file = Open("abc.txt", 1);
+    PrintString("fileID: ");
     PrintNum(file);
-    // PrintString("\n");
-    // Read(buffer1, 2, file);
-    // PrintString(buffer1);
+    PrintString("\n");
+    Read(buffer1, 19, file);
+    PrintString("Doc tu file: ");
+    PrintString(buffer1);
+    PrintString("\n");
+    Seek(1, file);
     Read(buffer1, 19, file);
     PrintString(buffer1);
     PrintString("\n");
     Close(file);
-    PrintNum(Write("20120028", 8, file));
-    PrintString("\n");
 
-    // char buffer[11]="";
-    // int fileID = Open("an.txt", 1);
-    // Seek(1, fileID);
-    // Read(buffer, 10, fileID);
-    // Close(fileID);
-    // PrintString(buffer2);
-    // PrintString("\n");
+    file = Open("an.txt", 1);
+    PrintString("fileID: ");
+    PrintNum(file);
+    PrintString("\n");
+    Read(buffer1, 19, file);
+    PrintString("Doc tu file: ");
+    PrintString(buffer1);
+    PrintString("\n");
+    Close(file);
 
     Halt();
 }
